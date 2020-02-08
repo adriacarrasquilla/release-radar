4f4c97afe0a1fc5bfd3f550db1a27812347db33f
Adrià Carrasquilla <carrasquilla.adria@gmail.com>
2020-02-08 12:50:07 +0100

    Delete secret.py

--- Scripts/secret.py
+++
@@ -1,9 +0,0 @@
-client_id = 'YOUR_CLIENT_ID'
-client_secret = 'YOUR_CLIENT_SECRET'
-redirect_uri = 'YOUR_APP_REDIRECT_URI'
-username = 'USER_ID'
-playlists = [{'name': 'example1', 'id': 'example1_id'},
-             {'name': 'example2', 'id': 'example2_id'}]
-playlistsNames = ['example1', 'example1']
-
-test
